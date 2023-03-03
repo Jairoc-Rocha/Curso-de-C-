8b7f68354b0e984fc08bf270751c0fdcd7ca0650
Jairo Rocha <jairoc.rocha10@gmail.com>
2023-03-03 08:13:50 -0300

    Criada a pasta EnumComp

---
+++ POO/Course/Course/EnumComp/EnumComp/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1 {
+    internal class Program {
+        static void Main(string[] args) {
+            //teste do clone do git e github
+        }
+    }
+}
