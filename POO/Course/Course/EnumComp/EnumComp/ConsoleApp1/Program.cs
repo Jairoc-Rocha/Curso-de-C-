9f27b4dbdffcd4977be081966a08aa1b5cca40fb
Jairo Rocha <jairoc.rocha10@gmail.com>
2023-03-03 11:30:33 -0300

    Estudando enums

--- POO/Course/Course/EnumComp/EnumComp/ConsoleApp1/Program.cs
+++ POO/Course/Course/EnumComp/EnumComp/ConsoleApp1/Program.cs
@@ -1,13 +1,32 @@
-﻿using System;
+﻿using ConsoleApp1.Entities;
+using ConsoleApp1.Entities.enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1 {
-    internal class Program {
-        static void Main(string[] args) {
-            //teste do clone do git e github
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment           
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString(); //Converte um enum para string
+            Console.WriteLine(txt);
+
+            OrderStatus os = (OrderStatus)Enum.Parse(typeof(OrderStatus), "Delivered");//Converte uma string para enum 
+            Console.WriteLine(os);
+
+            Console.ReadLine();
         }
     }
 }
