305c892142928dcf81de85b5428c849dbc3e8cab
Jairo Rocha <jairoc.rocha10@gmail.com>
2023-03-07 15:08:30 -0300

    Estudando Herança

---
+++ POO/Course/Course/Heranca/Heranca/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Heranca.Entities;
+
+namespace Heranca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAcount account = new BusinessAcount(8010, "Bob Brown", 100.00, 500.0);
+
+
+            Console.WriteLine(account.Balance);
+
+            Console.ReadLine();
+        }
+    }
+}
