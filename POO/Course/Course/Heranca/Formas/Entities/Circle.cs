b3a46e3f416f19436defb8bcd9b840a1ef9b57e8
Jairo Rocha <jairoc.rocha10@gmail.com>
2023-03-09 22:15:26 -0300

    Aprendendo sobre classes e métodos abstratos

---
+++ POO/Course/Course/Heranca/Formas/Entities/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using Formas.Entities.Enums;
+
+namespace Formas.Entities
+{
+    internal class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius, Color color) : base (color)
+        {
+            Radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
---
+++ POO/Course/Course/Heranca/Formas/Entities/Enums/Color.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Formas.Entities.Enums
+{
+    internal enum Color : int
+    {
+        Black = 0,
+        Blue = 1,
+        Red = 2
+    }
+}
